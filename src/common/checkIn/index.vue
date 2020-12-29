6bf420a0f7d08c0577bf6d0b83cc62d81950ca4b
李家朋 <accounts_5c921fd1bb0faf00185f2dd2@mail.teambition.com>
2020-12-29 17:12:45 +0800

    feat: 提交小写敏感

---
+++ src/common/checkIn/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <my-dialog v-model:visible="visible">
+    <div class="check-in">
+      <img
+        src="https://img1.halobear.com/wedding/FrCy7H_tfzhBI7dO9_Ya5qsI1eN9.png"
+        class="close"
+        @click="close()"
+        alt
+      />
+      <div class="title text">登录李小朋 (゜-゜)つロ</div>
+      <a-input
+        size="large"
+        placeholder="请输入用户名"
+        class="input"
+        v-model:value="payload.userName"
+      ></a-input>
+      <a-input
+        size="large"
+        placeholder="请输入密码"
+        class="input"
+        v-model:value="payload.password"
+      ></a-input>
+      <a-button type="primary" class="submit-btn" @click="onSubmit" size="large">登录</a-button>
+    </div>
+  </my-dialog>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import { Input, Button } from 'ant-design-vue';
+import myDialog from '../baseDialog';
+
+export default {
+  components: {
+    aButton: Button,
+    aInput: Input,
+    myDialog,
+  },
+  setup() {
+    let payload = reactive({
+      userName: 'lijiapeng',
+      password: '123456',
+    });
+    return {
+      payload,
+    };
+  },
+  data() {
+    return {
+      visible: false,
+      resolve: () => {},
+      reject: () => {},
+    };
+  },
+  methods: {
+    close() {
+      this.visible = false;
+      this.reject();
+    },
+    async onSubmit() {
+      await this.$store.dispatch('user/login', this.payload);
+      this.visible = false;
+      this.resolve();
+    },
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.check-in {
+  background-color: #fff;
+  width: 460px;
+  padding: 50px 40px 40px 40px;
+  border-radius: 4px;
+  position: relative;
+  .close {
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    top: 20px;
+    right: 20px;
+    cursor: pointer;
+  }
+  .title {
+    font-size: 20px;
+    font-weight: bold;
+    color: #222222;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .input {
+    margin-bottom: 18px;
+  }
+  .submit-btn {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/common/commonFoot/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="common-footer">
+    <div class="footer-box middle-flex">
+      <div class="title">©2020 李小朋</div>
+      <a href="http://www.beian.miit.gov.cn" class="num">豫ICP备18001833号-1</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='less' scoped>
+.common-footer {
+  background-color: #212121;
+  .footer-box {
+    width: 1190px;
+    margin: 0 auto;
+    height: 100px;
+    box-sizing: border-box;
+    color: #fff;
+    font-weight: 500;
+    justify-content: space-between;
+    .num {
+      color: #fff;
+    }
+  }
+}
+</style>
