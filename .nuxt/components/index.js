faa4a5f13eda64917e028bb1a9f73b0e62339130
李家朋 <accounts_5c921fd1bb0faf00185f2dd2@mail.teambition.com>
2021-02-04 11:11:26 +0800

    fix: 合并

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Index } from '../../components/index/index.vue'
-export { default as BlogDetail } from '../../components/blog-detail/index.vue'
-export { default as BlogList } from '../../components/index/components/blog-list.vue'
-export { default as RightPanel } from '../../components/index/components/right-panel.vue'
-
-export const LazyIndex = import('../../components/index/index.vue' /* webpackChunkName: "components/index/index'}" */).then(c => c.default || c)
-export const LazyBlogDetail = import('../../components/blog-detail/index.vue' /* webpackChunkName: "components/blog-detail/index'}" */).then(c => c.default || c)
-export const LazyBlogList = import('../../components/index/components/blog-list.vue' /* webpackChunkName: "components/index/components/blog-list'}" */).then(c => c.default || c)
-export const LazyRightPanel = import('../../components/index/components/right-panel.vue' /* webpackChunkName: "components/index/components/right-panel'}" */).then(c => c.default || c)
