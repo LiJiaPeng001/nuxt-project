059bdf0ac190787f88e8143e253c8d1eda1a7b1d
LiJiaPeng001 <1146417576@qq.com>
2021-03-09 10:46:18 +0800

    feat: 修改默认环境变量属性

--- contants/index.js
+++ contants/index.js
@@ -1,8 +1,10 @@
 export const imgUrl = 'https://img1.halobear.com/wedding/FlhPh9u0ypAlzbxey-YjolnrqT2h.jpg'
+const env = process.env.NODE_ENV
+// const env = 'production'
 
 export const apiMaps = {
   development: 'http://127.0.0.1:1202',
   production: 'https://www.ljpeng.fun',
 }
 
-export const baseURL = apiMaps[process.env.NODE_ENV]
+export const baseURL = apiMaps[env]
